edac50fa0c00ee9c1a01b596664667c7797184ec
Giale24 <108978110+Giale24@users.noreply.github.com>
2022-07-12 17:01:30 +0200

    Update math_functions.py

--- math_functions.py
+++ math_functions.py
@@ -5,11 +5,11 @@ x = 1
 y = 2
 z = 3
 
-print(round(pi))
-print(math.ceil(pi))
-print(math.floor(pi))
-print(abs(pi))
-print(pow(pi,2))
-print(math.sqrt(420))
+print(round(pi)) #arrotonda
+print(math.ceil(pi)) #arrotonda al numero più alto
+print(math.floor(pi)) #arrotonda al numero più basso
+print(abs(pi)) #valore assoluto
+print(pow(pi,2)) #potenza
+print(math.sqrt(420)) #radice quadrata
 print(max(x,y,z))
 print(min(x,y,z))
