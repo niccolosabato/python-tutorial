d550c163735874cb277d4bc20f276ec9e326a844
Giale24 <108978110+Giale24@users.noreply.github.com>
2022-07-12 17:22:03 +0200

    string_methods in Python

--- string_methods.py
+++ string_methods.py
@@ -1,7 +1,7 @@
 name = "Bro"
 
-print(len(name)) #technically this is a function
-print(name.find("o")) 
+print(len(name)) #technically this is a function, return the lenght of the string
+print(name.find("o")) # finds the first occurrence of the specified value.
 print(name.capitalize())
 print(name.upper())
 print(name.lower())
